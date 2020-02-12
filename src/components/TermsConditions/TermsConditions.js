9e8536ba67a0dcaf5e857bd95307e15e365115d4
Jamie Carter <jc2820@hotmail.com>
2020-02-12 12:15:48 +0000

    Add t&c to router Relates #3

--- src/components/TermsConditions/TermsConditions.js
+++ src/components/TermsConditions/TermsConditions.js
@@ -5,36 +5,45 @@ const TermsConditions = () => {
   return (
     <SC.TcBox>
       <h2>Terms & Conditions</h2>
-      
-        <h3>1. Interpretation</h3>
-        <p>
-        1.1 These Conditions together with the Licence
-        Confirmation constitute an agreement ("the Agreement") between the
-        British Museum Company Limited ("BMCo") and the person identified in the
-        Licence Confirmation ("Licensee"). All licences and permissions in the
-        Agreement are conditional upon Licensee complying with the terms of the
-        Agreement. Reproducing any photographs or other copyright work(s) in
-        breach of or outside the scope of the Agreement is an infringement of
-        copyright. 1.2 If there is any manifest conflict between the provisions
-        of the Licence Confirmation and those of these Conditions then the
-        provisions of the Licence Confirmation shall prevail. The Agreement
-        shall prevail over any terms which Licensee may purport to apply (e.g.
-        by way of a purchase order). 1.3 Unless otherwise stated in the Licence
-        Confirmation, Licensee enters into the Agreement as a business rather
-        than as a consumer. Please let us know if you are a consumer. 1.4 In the
-        Agreement the following terms, unless the context otherwise requires,
-        have the following meanings: "the Image" means any and all photographs
-        described in the Licence Confirmation together with any accompanying
-        caption text and other materials supplied by BMCo; "Licensed Product"
-        means (subject to Clause 3.7) the publication or type of product
+
+      <h3>1. Interpretation</h3>
+      <p>
+        1.1 These Conditions together with the Licence Confirmation constitute
+        an agreement (the Agreement) between the British Museum Company Limited
+        (BMCo) and the person identified in the Licence Confirmation (Licensee).
+        All licences and permissions in the Agreement are conditional upon
+        Licensee complying with the terms of the Agreement. Reproducing any
+        photographs or other copyright work(s) in breach of or outside the scope
+        of the Agreement is an infringement of copyright.
+      </p>
+      <p>
+        1.2 If there is any manifest conflict between the provisions of the
+        Licence Confirmation and those of these Conditions then the provisions
+        of the Licence Confirmation shall prevail. The Agreement shall prevail
+        over any terms which Licensee may purport to apply (e.g. by way of a
+        purchase order).
+      </p>
+      <p>
+        1.3 Unless otherwise stated in the Licence Confirmation, Licensee enters
+        into the Agreement as a business rather than as a consumer. Please let
+        us know if you are a consumer.
+      </p>
+      <p>
+        1.4 In the Agreement the following terms, unless the context otherwise
+        requires, have the following meanings: the Image means any and all
+        photographs described in the Licence Confirmation together with any
+        accompanying caption text and other materials supplied by BMCo; Licensed
+        Product means (subject to Clause 3.7) the publication or type of product
         (including books, cards, TV programmes and films) which makes use of the
-        Image in the way described in the Licence Confirmation. "Licensed
-        Territory" means the territory described in the Licence Confirmation.
-        1.5 In the Agreement, references to "include" and "including" are to be
+        Image in the way described in the Licence Confirmation. Licensed
+        Territory means the territory described in the Licence Confirmation.
+      </p>
+      <p>
+        1.5 In the Agreement, references to include and including are to be
         construed without limitation. References to person(s) include
         incorporated and unincorporated bodies or partnerships and include
-        reference to that person's successors and permitted assigns. Headings
-        are for convenience only and shall not affect the interpretation of the
+        reference to that persons successors and permitted assigns. Headings are
+        for convenience only and shall not affect the interpretation of the
         Agreement.
       </p>
     </SC.TcBox>
