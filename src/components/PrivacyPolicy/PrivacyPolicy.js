50c3d09c6d93c7c790e5f0470af7ae15051544f9
renatajarmova <renatajarmova@gmail.com>
2020-02-12 12:29:48 +0000

    Create privacy policy component and its tests
    Co-authored-by: Jamie <jc2820@hotmail.com>
    
    Relates #3

---
+++ src/components/PrivacyPolicy/PrivacyPolicy.js
@@ -0,0 +1,41 @@
+import React from "react";
+import * as SC from "./PrivacyPolicy.style";
+
+const PrivacyPolicy = () => {
+  return (
+    <SC.PrivacyBox>
+      <h1>Privacy Policy</h1>
+      <h2>Privacy Notice, Cookie Policy and Copyright</h2>
+      <p>
+        All materials on this website are subject to the laws of copyright. You
+        may not reproduce or distribute the images or any part of the images for
+        commercial purposes in any way without a licence from the Museum Images.
+        Copyright in all the images remains with The Trustees of the Museum and
+        the collections it represents. By entering the website, you are
+        indicating that you have read and accepted the above conditions.
+      </p>
+      <h2>If you are registering an account on the G2H website</h2>
+      <p>
+        We will process your personal data because we are entering into a
+        contract with you or because we need to take steps to enter into a
+        contract with you. Your personal data will be used by us to communicate
+        with you about, or to comply with our obligations under, that contract.
+        We will keep your personal data for the duration of copyright following
+        the completion of the contract. Please see our privacy policy for more
+        details about how we use your personal data and about your rights or
+        contact the Museums Data Protection Officer at info@g2h.com, telephone
+        020 0000 0000.
+      </p>
+      <h2>Use of Cookies</h2>
+      <p>
+        Our website uses local storage (such as cookies) in order to provide you
+        with the best possible experience and to allow you to make use of
+        certain functionality (such as being able to shop online). Further
+        information can be found in our Cookies Policy, a copy of which is
+        available on the Museums website.
+      </p>
+    </SC.PrivacyBox>
+  );
+};
+
+export default PrivacyPolicy;
