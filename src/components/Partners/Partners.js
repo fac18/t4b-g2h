c3f77cbd7598f263e9ed4bd113ce15001535eb4e
ReginaldJbeili <reginaldjbeili@gmail.com>
2020-02-14 11:40:35 +0000

    Create museumDataCall to get info from airtable
    Co-authored-by: renata <renatajarmova@gmail.com>
    
    Relates #4

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import * as SC from "../styles/Text.style";
 // import { Link } from "react-router-dom";
 
 const Partners = () => {
+  const [museumData, setMuseumData] = useState(null);
+
+  const museumDataCall = async () => {
+    await (await fetch("/.netlify/functions/getMuseumData/getMuseumData.js"))
+      .json()
+      .then(data => setMuseumData(data))
+      .catch(console.error);
+  };
+
+  React.useEffect(() => {
+    museumDataCall();
+  }, []);
   return (
     <SC.CenteredText>
+      <button onClick={museumDataCall}>Data</button>
+      {console.log(museumData)}
       <h2>Our Partners</h2>
       <p>Museum name</p>
       {/* <p>{record.fields.name}</p>
