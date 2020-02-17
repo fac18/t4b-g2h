f7530ca976859578247640f2e1e8dc9299534a7c
Jamie Carter <jc2820@hotmail.com>
2020-02-17 12:54:44 +0000

    Add link props for searched images to go to preview page
    Co-authored-by: reginald <reginaldjbeili@gmail.com>
    Relates #9

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,6 +1,7 @@
 import React from "react";
 import * as SC from "./Search.style.js";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const SearchPage = ({ searchResult }) => {
   if (!searchResult) return <h1>No data yet</h1>;
@@ -10,12 +11,30 @@ const SearchPage = ({ searchResult }) => {
   return (
     <SC.SearchStyle>
       {searchRecords.map(record => (
-        <SC.ContentContainer key={record.fields.image_id}>
-          <SC.ImgContainer>
-            <SC.ImgInContainer src={record.fields.url} />
-          </SC.ImgContainer>
-          <p data-testid="first">{record.fields.caption}</p>
-        </SC.ContentContainer>
+        <Link to={{ pathname: "/previewimage", 
+        searchImageProps: {
+          image_id: record.fields.image_id,
+          caption: record.fields.caption,
+          description: record.fields.description,
+          collection: record.fields.collection,
+          medium: record.fields.medium,
+          creator: record.fields.creator,
+          dateOfCreation: record.fields.create_date,
+          period: record.fields.period,
+          copyrightStatus: record.fields.copyright_status,
+          copyrightHolder: record.fields.copyright_holder,
+          credit: record.fields.credit,
+          keywords: record.fields.keywords,
+          imageUrl: record.fields.url
+        }
+        }} key={record.fields.image_id}>
+          <SC.ContentContainer key={record.fields.image_id}>
+            <SC.ImgContainer>
+              <SC.ImgInContainer src={record.fields.url} />
+            </SC.ImgContainer>
+            <p data-testid="first">{record.fields.caption}</p>
+          </SC.ContentContainer>
+        </Link>
       ))}
     </SC.SearchStyle>
   );
