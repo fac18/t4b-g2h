e22c76ad091789dbc6e2cc369d81c7400d182647
renatajarmova <renatajarmova@gmail.com>
2020-02-20 18:15:04 +0000

    Fix erroring tests

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as Text from "../styles/Text.style";
 import * as SC from "./Search.style.js";
 import PropTypes from "prop-types";
