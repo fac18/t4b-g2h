5386154d583b16c45089e55e5dd5aeb6c6d1bbb1
renatajarmova <renatajarmova@gmail.com>
2020-02-21 00:49:12 +0000

    Style labels and inputs in refine search bar

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -10,8 +10,14 @@ import ListItem from "@material-ui/core/ListItem";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import * as Btn from "../styles/Buttons.style";
 import * as SC from "./Filter.style";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import TextField from "@material-ui/core/TextField";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   list: {
     width: 250
   },
@@ -21,8 +27,15 @@ const useStyles = makeStyles({
   link: {
     textDecoration: "none",
     color: "black"
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2)
   }
-});
+}));
 
 export default function Filter({ searchResult, setFilteredResult }) {
   // could refactor all states to useReducer
@@ -92,117 +105,103 @@ export default function Filter({ searchResult, setFilteredResult }) {
       // onKeyDown={toggleDrawer(side, false)}
     >
       <List>
-        <ListItem>
-          <label id="collectionValue">Collection</label>
-          <input
-            type="text"
-            id="collectionValue"
-            value={collectionValue}
-            onChange={event => setCollectionValue(event.target.value)}
-          ></input>
-        </ListItem>
+        <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+          Collection
+        </InputLabel>
+        <TextField
+          id="standard-basic"
+          value={collectionValue}
+          onChange={event => setCollectionValue(event.target.value)}
+        ></TextField>
       </List>
-      <Divider />
       <List>
-        <ListItem>
-          <label id="artistValue">Artist/Creator</label>
-          <input
-            type="text"
-            id="artistValue"
-            value={artistValue}
-            onChange={event => setArtistValue(event.target.value)}
-          ></input>
-        </ListItem>
+        <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+          Artist/Creator
+        </InputLabel>
+        <TextField
+          id="standard-basic"
+          value={artistValue}
+          onChange={event => setArtistValue(event.target.value)}
+        ></TextField>
       </List>
-      <Divider />
       <List>
-        <ListItem>
-          <label id="periodValue">Period/Century</label>
-          <input
-            type="text"
-            id="periodValue"
-            value={periodValue}
-            onChange={event => setPeriodValue(event.target.value)}
-          ></input>
-        </ListItem>
+        <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+          Period/Century
+        </InputLabel>
+        <TextField
+          id="standard-basic"
+          value={periodValue}
+          onChange={event => setPeriodValue(event.target.value)}
+        ></TextField>
       </List>
-      <Divider />
       <List>
-        <ListItem>
-          <label htmlFor="colourValue" id="colourValue">
-            Colour/B&W
-          </label>
-          <select
-            type="select"
-            id="colourValue"
-            value={colourValue}
-            onChange={event => setColourValue(event.target.value)}
-          >
-            <option value="" disabled selected>
-              Select an option
-            </option>
-            <option value="">None</option>
-            <option value="Colour">Colour</option>
-            <option value="B&W">Black & White</option>
-          </select>
-        </ListItem>
+        <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+          Medium
+        </InputLabel>
+        <TextField
+          id="standard-basic"
+          value={mediumValue}
+          onChange={event => setMediumValue(event.target.value)}
+        ></TextField>
       </List>
-      <Divider />
-      <List>
-        <ListItem>
-          <label htmlFor="orientationValue" id="orientationValue">
-            Orientation
-          </label>
-          <select
-            type="select"
-            id="orientationValue"
-            value={orientationValue}
-            onChange={event => setOrientationValue(event.target.value)}
-          >
-            <option value="" disabled selected>
-              Select an option
-            </option>
-            <option value="">None</option>
-            <option value="Landscape">Landscape</option>
-            <option value="Portrait">Portrait</option>
-            <option value="Square">Square</option>
-          </select>
-        </ListItem>
-      </List>
-      <Divider />
-      <List>
-        <ListItem>
-          <label htmlFor="peopleValue" id="peopleValue">
-            People
-          </label>
-          <select
-            type="select"
-            id="peopleValue"
-            value={peopleValue}
-            onChange={event => setPeopleValue(event.target.value)}
-          >
-            <option value="" disabled selected>
-              Select an option
-            </option>
-            <option value="">None</option>
-            <option value="Yes">Yes</option>
-            <option value="No">No</option>
-          </select>
-        </ListItem>
-      </List>
-      <Divider />
-      <List>
-        <ListItem>
-          <label id="mediumValue">Medium</label>
-          <input
-            type="text"
-            id="mediumValue"
-            value={mediumValue}
-            onChange={event => setMediumValue(event.target.value)}
-          ></input>
-        </ListItem>
-      </List>
-      <Divider />
+      <FormControl className={classes.formControl}>
+        <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+          Colour/B&W
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
+          type="select"
+          value={colourValue}
+          displayEmpty
+          onChange={event => setColourValue(event.target.value)}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value="Colour">Colour</MenuItem>
+          <MenuItem value="B&W">Black & White</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+          Orientation
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
+          displayEmpty
+          type="select"
+          value={orientationValue}
+          onChange={event => setOrientationValue(event.target.value)}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value="Landscape">Landscape</MenuItem>
+          <MenuItem value="Portrait">Portrait</MenuItem>
+          <MenuItem value="Square">Square</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel shrink id="demo-simple-select-placeholder-label-label">
+          People
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-outlined-label"
+          id="demo-simple-select-outlined"
+          displayEmpty
+          type="select"
+          value={peopleValue}
+          onChange={event => setPeopleValue(event.target.value)}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value="Yes">Yes</MenuItem>
+          <MenuItem value="No">No</MenuItem>
+        </Select>
+      </FormControl>
       <List>
         <Btn.SecondaryBtn onClick={clearFilters}>
           Clear filters
